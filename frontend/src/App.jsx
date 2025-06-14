9136ab044a410f32f48b69cfd4e66d7fae5f4461
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-06-14 20:15:58 +0800

    Add Hero component and integrate it into Home page
    - Created Hero component for showcasing vacation-ready outfits with a background image and call-to-action button.
    - Integrated Hero component into the Home page for improved user engagement and visual appeal.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<UserLayout />}>
-                    {/* User Layout */}
+                    <Route index element={<Home />} />
                 </Route>
                 <Route>{/* Admin Layout */}</Route>
             </Routes>
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Common/Header";
 import Footer from "../Common/Footer";
+import { Outlet } from "react-router-dom";
 
 const UserLayout = () => {
     return (
@@ -8,6 +9,9 @@ const UserLayout = () => {
             {/* Header */}
             <Header />
             {/* Main content */}
+            <main>
+                <Outlet />
+            </main>
             {/* Footer */}
             <Footer />
         </>
