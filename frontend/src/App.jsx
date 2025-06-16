42a4f20068910bcdabe1f4770e37aa8407360753
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-06-16 22:32:31 +0800

    feat: Add Profile page and integrate MyOrdersPage component; update routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import { Toaster } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                     <Route index element={<Home />} />
                     <Route path="login" element={<Login />} />
                     <Route path="register" element={<Register />} />
+                    <Route path="profile" element={<Profile />} />
                 </Route>
                 <Route>{/* Admin Layout */}</Route>
             </Routes>
