23ad52b8f6593ce446fe81025ed169b72b111a12
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-06-19 20:52:46 +0800

    feat: Add OrderConfirmationPage component and update routing in App; enhance CartDrawer to toggle cart on checkout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/Products/ProductDetails";
 import Checkout from "./components/Cart/Checkout";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 
 const App = () => {
     return (
@@ -25,6 +26,10 @@ const App = () => {
                     />
                     <Route path="product/:id" element={<ProductDetails />} />
                     <Route path="checkout" element={<Checkout />} />
+                    <Route
+                        path="order-confirmation"
+                        element={<OrderConfirmationPage />}
+                    />
                 </Route>
                 <Route>{/* Admin Layout */}</Route>
             </Routes>
